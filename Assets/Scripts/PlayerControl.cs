3bb4d3951a066bed550267ea49ac7121910f6bb2
Willem <willem@willem>
2017-11-13 13:42:24 +0100

    Fire 1 actie = duwen + rotatie fix + speed fix

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class PlayerControl : MonoBehaviour {
 
-	public float moveSpeed;
+	public float moveSpeed = 0.5f;
+	public int characterStrength = 250;
 	public string controllerNumber;
 
 	private Rigidbody rigidBody;
@@ -24,8 +25,24 @@ public class PlayerControl : MonoBehaviour {
 		rigidBody.velocity = rigidBody.velocity + moveVelocity;
 
 		// slerp model in de richting van beweging
-		if (moveInput != Vector3.zero) {
-			transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (moveInput.normalized), 0.2f);
+		if(moveInput != Vector3.zero) transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(moveInput.normalized), 0.2f);
+
+		if (Input.GetButtonDown("Fire1" + controllerNumber))
+		{
+			//Debug.Log("HIT");
+			//other.gameObject.GetComponent<Rigidbody> ().AddForce(this.transform.forward * 5000, ForceMode.Force);
+			//other.gameObject.GetComponent<Rigidbody> ().velocity = this.transform.forward * 500;
+			//this.rigidBody.AddForce(this.transform.forward * 100, ForceMode.Acceleration);
+		}
+	}
+
+	void OnTriggerStay (Collider other)
+	{
+		if (Input.GetButtonDown("Fire1" + controllerNumber) && other.tag == "Player" && other.GetComponent<Rigidbody>() != rigidBody)
+		{
+			Debug.Log("HIT enemy");
+			other.gameObject.GetComponent<Rigidbody> ().AddForce(this.transform.forward * characterStrength, ForceMode.Acceleration);
+			//other.gameObject.GetComponent<Rigidbody> ().velocity = this.transform.forward * 500;
 		}
 	}
 }
