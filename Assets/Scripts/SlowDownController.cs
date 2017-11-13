52a35972d7f93c40d3eb74cca12e20629ee07806
Jelle Braat <jelle@gamebasics.nl>
2017-11-13 17:03:44 +0100

    Added vomit/slowdown

---
+++ Assets/Scripts/SlowDownController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowDownController : MonoBehaviour {
+    public int newMaxSpeed;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player") {
+            var player = other.GetComponent<PlayerControl>().maxSpeed = newMaxSpeed;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player") {
+            var player = other.GetComponent<PlayerControl>().maxSpeed = 6;
+        }
+    }
+}
