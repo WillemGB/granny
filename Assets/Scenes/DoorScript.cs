49ed6d8e023f0a4ac36bf4209877a07beb036db5
GBWS18\Mark <markJJE@hotmail.com>
2017-11-13 16:14:20 +0100

    Added animation for doors. To do: when the designers made the doors, implement the animation.

---
+++ Assets/Scenes/DoorScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    Animator anim;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        anim.SetBool("OpenDoor", true);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        anim.enabled = true;
+        anim.SetBool("OpenDoor", false);
+    }
+
+    void onPauseAnimation()
+    {
+        anim.enabled = false;
+    }
+}
