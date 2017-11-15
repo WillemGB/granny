be92351750a8328fdf14b6c552820ef62f4bf254
Rob <rob@rob.nl>
2017-11-15 08:39:02 +0100

    Yes mijn eerste commit!

---
+++ Assets/Scripts/ItemDatabase.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class ItemDatabase : MonoBehaviour {
+
+    public List<Item> items = new List<Item>();
+
+    void Start()
+    {
+        items.Add(new Item("Apple",0,"een appel", 2 ,0, Item.ItemType.Power));
+    }
+    
+}
