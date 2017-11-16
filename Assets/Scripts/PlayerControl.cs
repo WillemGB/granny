b3093abc52cef8485c6ab269412bb1043db4702c
Jeroen Klarenbeek <jeroenk@gamebasics.nl>
2017-11-16 10:42:05 +0100

    Bed wordt nu altijd losgelaten

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -104,7 +104,7 @@ public class PlayerControl : MonoBehaviour {
 			Component[] comps = other.gameObject.GetComponents(typeof(InteractionInterface));
 			foreach (Component com in comps) {
 				var interactableScript = com as InteractionInterface;
-				interactableScript.onUseStart(this.gameObject);
+				interactableScript.onUse(this.gameObject);
 				var itemId = interactableScript.loot();
 				if ((controllerNumber == "" || controllerNumber == "2") && itemId == 1) {
 					inventory.AddItem (itemId);
@@ -117,15 +117,7 @@ public class PlayerControl : MonoBehaviour {
 				}
 			}
         }
-        if (other.tag == "Interactable" && Input.GetButtonUp("Fire1" + controllerNumber)) //button released
-        {
-            Component[] comps = other.gameObject.GetComponents(typeof(InteractionInterface));
-            foreach (Component com in comps)
-            {
-                var interactableScript = com as InteractionInterface;
-                interactableScript.onUseStop(this.gameObject);
-            }
-        }
+
     }
 
     void Animate()
