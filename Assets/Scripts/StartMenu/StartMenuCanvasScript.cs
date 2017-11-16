54e99843b1c3d0677eca77d40f66550106ac2d1d
Jelle Braat <jelle@gamebasics.nl>
2017-11-16 13:33:14 +0100

    Added Start menu and changed build settings

---
+++ Assets/Scripts/StartMenu/StartMenuCanvasScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class StartMenuCanvasScript : MonoBehaviour {
+    public Image BackgroundImage;
+    public Button StartButton;
+    public Button SettingsButton;
+    public Button BackButton;
+
+	// Use this for initialization
+	void Start () {
+        StartButton = StartButton.GetComponent<Button>();    	
+        SettingsButton = SettingsButton.GetComponent<Button>();    	
+        BackButton = BackButton.GetComponent<Button>();
+
+        // Only show back button in store
+        BackButton.enabled = false;
+	}
+	
+    public void StartGame()
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void ShowSettingsMenu()
+    {
+
+    }
+
+    public void ShowStartMenu()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/StartMenu/StartMenuScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.anyKeyDown)
+	        StartGame();
+	}
+
+    void StartGame()
+    {
+        Debug.Log("start game load scene");
+        Application.LoadLevel(1);
+    }
+}
