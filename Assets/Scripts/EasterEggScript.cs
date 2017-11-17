93101c984615dbb79e8345d8c7128d63c9fb4ab0
Jelle Braat <jelle@gamebasics.nl>
2017-11-17 10:53:03 +0100

    Trees!

---
+++ Assets/Scripts/EasterEggScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EasterEggScript : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (Collider.tag == "Player")
+            Application.LoadLevel(2);
+    }
+}
